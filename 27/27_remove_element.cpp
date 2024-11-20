0787f8c93763c2a0a0fe9b0dbe5201369c8b737c
Windows NT <nmt2400@g.rit.edu>
2024-11-19 19:54:24 -0500

    Update 27_remove_element.cpp

--- 27/27_remove_element.cpp
+++ 27/27_remove_element.cpp
@@ -6,9 +6,12 @@ public:
         for (int i = 0; i < nums.size(); ++i) {
             if (nums.at(i) != val) {
                 if (ret != i) {
-                    auto tmp = nums.at(ret);
                     nums[ret] = nums.at(i);
-                    nums[i] = nums[ret];
+                    // was swapping, but we can cheat because we know what the
+                    // value we are swapping with is going to be...
+                    // Actually, we dont even need to swap based on the how this is evaluated because it only
+                    // looks at the first ret values
+                    // nums[i] = val;
                 }
 
                 ++ret;
