dc7a2b44675914ec9eb81410a31e8050abf5bba3
Windows NT <nmt2400@g.rit.edu>
2024-11-19 20:38:47 -0500

    Update 27_remove_element.cpp

--- 27/27_remove_element.cpp
+++ 27/27_remove_element.cpp
@@ -2,22 +2,12 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int ret = 0;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums.at(i) != val) {
-                if (ret != i) {
-                    nums[ret] = nums.at(i);
-                    // was swapping, but we can cheat because we know what the
-                    // value we are swapping with is going to be...
-                    // Actually, we dont even need to swap based on the how this is evaluated because it only
-                    // looks at the first ret values
-                    // nums[i] = val;
-                }
-
-                ++ret;
+        const auto sz = nums.size();
+        for (auto i = 0u; i < sz; ++i) {
+            if (nums[i] != val) {
+                nums[ret++] = nums[i];
             }
         }
-
         return ret;
     }
 };
